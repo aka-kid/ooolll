192e1e82f1a4e5f2c08041a2f36041b2ffa502e9
aka-kid <79195636+aka-kid@users.noreply.github.com>
2022-07-24 15:52:12 +0800

    Update rlsp.py

--- rlsp.py
+++ rlsp.py
@@ -1,6 +1,7 @@
 #-- coding: utf-8 --
 import io
 import sys
+from h11 import Data
 #改变标准输出的默认编码
 sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 import requests
@@ -19,6 +20,7 @@ def get_cookie():
     page = str(random.randint(0,5499))      # 页码范围为随机数，因为指定了行数（list_rows）为1，所以生成随机页码后，即可获得随机视频id
     url = f"https://ranlv.lvfacn.com/api.php/Ranlv/index?access_token={access_token}&category=19&list_rows=1&member_id=1408390&page="+ page +f"&user_token={user_token}"
     header = {
+        'Referer': "https://ran.lvfacn.com/pages/rank/prizeapp?",
         "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
     }
     resp = requests.post(url=url, headers=header)
@@ -65,6 +67,7 @@ def comment():
 
 # 投票任务
 def goVote():
+    global charts_id
     # 获取榜单信息
     title_id = str(random.randint(0,100))
     list_url = f"https://ranlv.lvfacn.com/api.php/Rcharts/getRank?access_token={access_token}&basis=0&id={title_id}&list_rows=1&member_id=1408390&page=1&ran=1&user_token={user_token}"
@@ -73,16 +76,44 @@ def goVote():
     # 获取榜单id
     for i in dic['data']['data']:
         list_id = i['id']
-    t = random.randint(3,11)  # 随机时间操作
-    print("随机等待->>" + str(t) + "秒\n")
-    time.sleep(t)
     # 获取投票id
     charts_id = str(random.randint(0,100))
     vote_url = f"https://ranlv.lvfacn.com/api.php/Rcharts/goVote?access_token={access_token}&charts_id={charts_id}&is_act=1&member_id=1408390&num=1&user_token={user_token}&video_id={list_id}"
     resp = requests.get(url=vote_url, headers=header).json()
+    t = random.randint(3,11)  # 随机时间操作
+    print("随机等待->>" + str(t) + "秒\n")
+    time.sleep(t)
     print(f'\n投票结果:' + resp['msg'])
-    money_rank()
+    share()
 
+# 分享任务
+def share():
+    url = f'https://ranlv.lvfacn.com/api.php/Rcharts/shareVideo?access_token={access_token}&member_id=1408390&user_token={user_token}&video_id={charts_id}'
+    headers = {
+        'User-Agent': 'ran lu shi pin/1.0.79 (iPhone; iOS 14.8.1; Scale/3.00)'
+    }
+    resp = requests.post(url=url, headers=headers).json()
+    user_info()
+
+# # 榜单任务
+# def myVote():
+#     url = 'https://ranlv.lvfacn.com/api.php/Rcharts/myVotes'
+#     data = "{\"access_token\":\"f2e1d57418bb3d6a0439b494f3468a75\",\"user_token\":\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJSYW5sdiBKV1QiLCJpYXQiOjE2NDg2MDE5NTcsImV4cCI6MzI5NzIwMzkxNCwiYXVkIjoiUmFubHYiLCJzdWIiOiJSYW5sdiIsImRhdGEiOnsibWVtYmVyX2lkIjoxNDA4MzkwLCJhdmF0YXIiOiJodHRwOlwvXC9yYW5sdi5sdmZhY24uY29tXC9zdGF0aWNcL21vZHVsZVwvYWRtaW5cL2ltZ1wvZGVmYXVsdF9oZWFkLmpwZyIsIm5pY2tuYW1lIjoiXHU3OTVlXHU5YTZjXHU2MGM1XHU1MWI1IiwibW9iaWxlIjoiMTM5MTEwMjk5MjMiLCJpc19jb3JwIjowfX0.mptkcU-dY6r1vUPKM7mC8hl4OgorPp9GSfZ_3x9u2jQ\"}"
+#     resp = requests.post(url=url, headers=header, data=data)
+#     print(resp.json())
+
+#  账号信息
+def user_info():
+    url = f'https://ranlv.lvfacn.com/api.php/Member/mine?access_token={access_token}&member_id=1408390&user_id=1408390&user_token={user_token}'
+    resp = requests.get(url=url, headers=header)
+    dic = resp.json()
+    # 昵称
+    nickname = str(dic['user']['nickname'])
+    # 账户余额
+    balance = str(dic['user']['balance'])  
+    print(f'\n账号名称：' + nickname + f'\n余额：' + balance)
+    time.sleep(1)
+    money_rank()
 
 # 获取赚钱任务进度
 def money_rank():
@@ -102,10 +133,6 @@ def money_rank():
     # 投票任务进度
     goVote_result = str(dic['data'][5]['title']) + f'进度:' + str(dic['data'][5]['to_num']) + f'/' + str(dic['data'][5]['num'])
     print(f"\n任务进度列表：" + "\n" + release_result + "\n" + fabulous_result + "\n" + comment_result + "\n" + data_result + "\n" + video_result + "\n" + goVote_result)
-
-# #  获取提现任务进度
-# def task_rank():
-#     url = f"https://ranlv.lvfacn.com/api.php/Share/taskCenter?access_token={access_token}&member_id=1408390&user_token={user_token}"
 
 def main() :
     get_cookie()
